10c003af6690a9371f3c0cc7ee42f94fea4bd3c5
GustavParment <gustav.parment1993@gmail.com>
2023-09-20 12:51:55 +0200

    uppgift#4

--- src/com/GUSTAV/demo/Main.java
+++ src/com/GUSTAV/demo/Main.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        boolean isPlaying = true;
 
 
         do {
-            System.out.println("ENTER A NUMBER");
-            sc.nextInt();
+            System.out.println("\u001b[32m"+"PRESS 'ENTER' TO CONTINUE THE LOOP" + "\u001b[0m"+
+                    "\u001B[31m"+"\nOR TYP EXIT TO STOP LOOP" + "\u001B[0m");
+            String result = sc.nextLine();
 
-        }while(true );
+            if (result.equalsIgnoreCase("stop")){
+                break;
+            }
+
+
+
+        }while(isPlaying );
 
 
 
