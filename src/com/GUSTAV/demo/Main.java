e16bd38cfb29d56296e03fcd8f879094718bbe94
GustavParment <gustav.parment1993@gmail.com>
2023-09-20 12:07:14 +0200

    uppgift#3

--- src/com/GUSTAV/demo/Main.java
+++ src/com/GUSTAV/demo/Main.java
@@ -5,17 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x;
-
-        System.out.println("Enter a number: ");
-        x = sc.nextInt();
-
-        for (int i = 0; i < x ; i++) {
-            System.out.println("The loop will run " + x + " times");
-
-        }
 
 
+        do {
+            System.out.println("ENTER A NUMBER");
+            sc.nextInt();
+
+        }while(true );
 
 
 
